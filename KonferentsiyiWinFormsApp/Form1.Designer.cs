3866d54c90516667f65ed317a6c77c1691ad47f4
DeadInside <DeadInside@DESKTOP-POAUPA8>
2023-12-08 11:02:09 +0200

    first commit

--- KonferentsiyiWinFormsApp/Form1.Designer.cs
+++ KonferentsiyiWinFormsApp/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.довідникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.конференціїToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.звітиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.довідникиToolStripMenuItem,
+            this.конференціїToolStripMenuItem,
+            this.звітиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вихідToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
+            // 
+            // довідникиToolStripMenuItem
+            // 
+            this.довідникиToolStripMenuItem.Name = "довідникиToolStripMenuItem";
+            this.довідникиToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.довідникиToolStripMenuItem.Text = "Довідники";
+            // 
+            // конференціїToolStripMenuItem
+            // 
+            this.конференціїToolStripMenuItem.Name = "конференціїToolStripMenuItem";
+            this.конференціїToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.конференціїToolStripMenuItem.Text = "Конференції";
+            // 
+            // звітиToolStripMenuItem
+            // 
+            this.звітиToolStripMenuItem.Name = "звітиToolStripMenuItem";
+            this.звітиToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.звітиToolStripMenuItem.Text = "Звіти";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "АІС Конференції";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem довідникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem конференціїToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem звітиToolStripMenuItem;
     }
 }
 
--- KonferentsiyiWinFormsApp/Form1.cs
+++ KonferentsiyiWinFormsApp/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
 
 namespace KonferentsiyiWinFormsApp
 {
     public partial class Form1 : Form
     {
+        static private MySqlConnection con = new MySqlConnection();
+        private MySqlCommand cmd;
+
         public Form1()
         {
+            this.Text = "АІС Конференції";
+            this.WindowState = FormWindowState.Maximized;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            
             InitializeComponent();
+
+            try
+            {
+                con.ConnectionString = "server=localhost;User Id=root;Persist Security Info=True;database=konferentsiyi_bohdanets_";
+                con.Open();
+                MessageBox.Show("Підключення відбулось успішно!");
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Errror: " + ex.Message);
+            }
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Ви впевненні, що хочете завершити роботу?", "Вихід", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
     }
 }
